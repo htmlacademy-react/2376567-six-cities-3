7399eb43be7f3fe7d7ba08577eed26fe5c66c589
SergeyMedved <serezham13@gmail.com>
2025-05-27 15:21:39 +0700

    добавил метод axios.create

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,13 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
 const TIME_OUT = 5000;
 const BASE_URL = 'https://15.design.htmlacademy.pro/six-cities';
 
-export const API = axios({
-  baseURL: BASE_URL,
-  timeout: TIME_OUT,
-});
+export const createAPI = ():AxiosInstance => {
+  const api = axios.create({
+    baseURL: BASE_URL,
+    timeout: TIME_OUT,
+  });
+
+  return api;
+};

9c3bb12f43edc19154ed18ef5d047dd7c487dd19
SergeyMedved <serezham13@gmail.com>
2025-05-27 15:57:13 +0700

    подключил thunk к store

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,10 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cityReducer from './citySlice';
 import offersReducer from './offersSlice';
+import { createAPI } from '../api';
+
+export const api = createAPI();
 
 export const store = configureStore({
   reducer: {
     city: cityReducer,
     offers: offersReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
 });
